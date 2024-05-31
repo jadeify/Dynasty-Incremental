f343bd3827d0aa4b34f077622cad95777180942f
jadeify <william.a.ramming@gmail.com>
2024-05-31 10:09:02 -0400

    switched fully to using break_infinity.js for all number storage, also started using the antimatter dimensions notation for all numbers which are displayed. as well as starting on fixing the formatting of the website in css, not totally finished yet.

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,56 +1,52 @@
-const Decimal = require('break_infinity.js');
+import Decimal from "break_infinity.js";
 import * as ADNotations from '@antimatter-dimensions/notations';
 const mixedScientific=new ADNotations.MixedScientificNotation();
 let resources={
-	money:{amt:15},
-	wheat:{amt:0,er:2},
-	cotton:{amt:0,er:5},
-	stone:{amt:0,er:3},
-	metal:{amt:0,er:7},
-	coal:{amt:0,er:4}
+	money:{amt:new Decimal(15)},
+	wheat:{amt:new Decimal(0),er:new Decimal(2)},
+	cotton:{amt:new Decimal(0),er:new Decimal(5)},
+	stone:{amt:new Decimal(0),er:new Decimal(3)},
+	metal:{amt:new Decimal(0),er:new Decimal(7)},
+	coal:{amt:new Decimal(0),er:new Decimal(4)}
 };
-let previousResources={money:15,wheat:0,cotton:0,stone:0,metal:0,coal:0};
+let previousResources={money:new Decimal(15),wheat:new Decimal(0),cotton:new Decimal(0),stone:new Decimal(0),metal:new Decimal(0),coal:new Decimal(0)};
 let buildings={
-	farm:{amt:0,price:15,rate1:0.15,rate2:0.07},
-	mine:{amt:0,price:100,rate1:0.13,rate2:0.06,rate3:0.1}
+	farm:{amt:new Decimal(0),price:new Decimal(15),rate1:new Decimal(0.15),rate2:new Decimal(0.07)},
+	mine:{amt:new Decimal(0),price:new Decimal(100),rate1:new Decimal(0.13),rate2:new Decimal(0.06),rate3:new Decimal(0.1)}
 };
 let employees=[{intelligence:0}];
 export function buyBuilding(building){
-	if(resources.money.amt>=buildings[building].price){
-		resources.money.amt-=buildings[building].price;
-		buildings[building].price=Math.round(buildings[building].price*1.2);
-		buildings[building].amt++;
+	if(resources.money.amt.greaterThanOrEqualTo(buildings[building].price)){
+		resources.money.amt=resources.money.amt.minus(buildings[building].price);
+		buildings[building].price=buildings[building].price.times(1.2).round();
+		buildings[building].amt=buildings[building].amt.add(1);
 	}
-}
-function roundTo(num, precision){
-	const factor=Math.pow(10,precision);
-	return Math.round(num*factor)/factor;
 }
 export function buyWorker(){
 
 }
 function updateValues(){
-	document.getElementById("money").innerHTML=roundTo(resources.money.amt,2);
-	document.getElementById("wheat").innerHTML=roundTo(resources.wheat.amt,2);
-	document.getElementById("cotton").innerHTML=roundTo(resources.cotton.amt,2);
-	document.getElementById("stone").innerHTML=roundTo(resources.stone.amt,2);
-	document.getElementById("metal").innerHTML=roundTo(resources.metal.amt,2);
-	document.getElementById("coal").innerHTML=roundTo(resources.coal.amt,2);
-	document.getElementById("farmCost").innerHTML=buildings.farm.price+" Money";
-	document.getElementById("farms").innerHTML=buildings.farm.amt+" Farms";
-	document.getElementById("mineCost").innerHTML=buildings.mine.price+" Money";
-	document.getElementById("mines").innerHTML=buildings.mine.amt+" Mines";
-	document.getElementById("moneyPS").innerHTML=roundTo((resources.money.amt-previousResources.money)*20,2);
-	document.getElementById("wheatPS").innerHTML=roundTo((resources.wheat.amt-previousResources.wheat)*20,2);
-	document.getElementById("cottonPS").innerHTML=roundTo((resources.cotton.amt-previousResources.cotton)*20,2);
-	document.getElementById("stonePS").innerHTML=roundTo((resources.stone.amt-previousResources.stone)*20,2)
-	document.getElementById("metalPS").innerHTML=roundTo((resources.metal.amt-previousResources.metal)*20,2);
-	document.getElementById("coalPS").innerHTML=roundTo((resources.coal.amt-previousResources.coal)*20,2);
+	document.getElementById("money").innerHTML=mixedScientific.format(resources.money.amt,2,2);
+	document.getElementById("wheat").innerHTML=mixedScientific.format(resources.wheat.amt,2,2);
+	document.getElementById("cotton").innerHTML=mixedScientific.format(resources.cotton.amt,2,2);
+	document.getElementById("stone").innerHTML=mixedScientific.format(resources.stone.amt,2,2);
+	document.getElementById("metal").innerHTML=mixedScientific.format(resources.metal.amt,2,2);
+	document.getElementById("coal").innerHTML=mixedScientific.format(resources.coal.amt,2,2);
+	document.getElementById("farmCost").innerHTML=mixedScientific.format(buildings.farm.price,2,2)+" Money";
+	document.getElementById("farms").innerHTML=mixedScientific.format(buildings.farm.amt,0,0)+" Farms";
+	document.getElementById("mineCost").innerHTML=mixedScientific.format(buildings.mine.price,2,2)+" Money";
+	document.getElementById("mines").innerHTML=mixedScientific.format(buildings.mine.amt,0,0)+" Mines";
+	document.getElementById("moneyPS").innerHTML=mixedScientific.format((resources.money.amt.minus(previousResources.money).times(20)),2,2);
+	document.getElementById("wheatPS").innerHTML=mixedScientific.format((resources.wheat.amt.minus(previousResources.wheat).times(20)),2,2);
+	document.getElementById("cottonPS").innerHTML=mixedScientific.format((resources.cotton.amt.minus(previousResources.cotton).times(20)),2,2);
+	document.getElementById("stonePS").innerHTML=mixedScientific.format((resources.stone.amt.minus(previousResources.stone).times(20)),2,2);
+	document.getElementById("metalPS").innerHTML=mixedScientific.format((resources.metal.amt.minus(previousResources.metal).times(20)),2,2);
+	document.getElementById("coalPS").innerHTML=mixedScientific.format((resources.coal.amt.minus(previousResources.coal).times(20)),2,2);
 }
 export function sellResources(){
 	for (let i = 1; i < Object.keys(resources).length; i++) {
-		resources.money.amt+=(resources[Object.keys(resources)[i]].amt*resources[Object.keys(resources)[i]].er);
-		resources[Object.keys(resources)[i]].amt=0;
+		resources.money.amt=resources.money.amt.add((resources[Object.keys(resources)[i]].amt*resources[Object.keys(resources)[i]].er));
+		resources[Object.keys(resources)[i]].amt=new Decimal(0);
 	}
 }
 function storePastValues(){
@@ -63,11 +59,11 @@ export function changeWorkerAmount(building,amount){
 }
 setInterval(function(){
 	storePastValues();
-	resources.wheat.amt+=buildings.farm.amt*buildings.farm.rate1/20;
-	resources.cotton.amt+=buildings.farm.amt*buildings.farm.rate2/20;
-	resources.stone.amt+=buildings.mine.amt*buildings.mine.rate1/20;
-	resources.metal.amt+=buildings.mine.amt*buildings.mine.rate2/20;
-	resources.coal.amt+=buildings.mine.amt*buildings.mine.rate3/20;
-	document.title = "$"+roundTo(resources.money.amt,1)+" - Dynasty Incremental";
+	resources.wheat.amt=resources.wheat.amt.add(buildings.farm.amt.times(buildings.farm.rate1.dividedBy(20)));
+	resources.cotton.amt=resources.cotton.amt.add(buildings.farm.amt.times(buildings.farm.rate2.dividedBy(20)));
+	resources.stone.amt=resources.stone.amt.add(buildings.mine.amt.times(buildings.mine.rate1.dividedBy(20)));
+	resources.metal.amt=resources.metal.amt.add(buildings.mine.amt.times(buildings.mine.rate2.dividedBy(20)));
+	resources.coal.amt=resources.coal.amt.add(buildings.mine.amt.times(buildings.mine.rate3.dividedBy(20)));
+	document.title = "$"+mixedScientific.format(resources.money.amt,2,2)+" - Dynasty Incremental";
 	updateValues();
 },50);
